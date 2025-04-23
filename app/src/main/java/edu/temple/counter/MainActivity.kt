f8051e8feac196acc8cc51266e3459b79368b889
tun93460 <ldgraham2002@gmail.com>
2025-04-23 15:22:35 -0400

    Added buttons to increment and decrement the count

--- app/src/main/java/edu/temple/counter/MainActivity.kt
+++ app/src/main/java/edu/temple/counter/MainActivity.kt
@@ -8,11 +8,17 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role.Companion.Button
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -38,10 +44,20 @@ class MainActivity : ComponentActivity() {
 fun Counter(modifier: Modifier = Modifier) {
     Column (modifier = modifier.fillMaxSize(), verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally) {
+        var count: Int by rememberSaveable { mutableStateOf(0) }
+
+        Button(onClick = { count += 1 }) {
+            Text(text = "+")
+        }
+
         Text(
-            text = "0",
-            fontSize = 192.sp
+            text = count.toString(),
+            fontSize = 192.sp,
         )
+
+        Button(onClick = { count -= 1 }) {
+            Text(text = "-")
+        }
     }
 }
 
